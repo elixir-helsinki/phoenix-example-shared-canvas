4e4cde448d09e417411d35738dd12245cc683039
Roope Kangas <muhmi@iki.fi>
2015-04-25 19:09:59 +0300

    handle click

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,9 +2,16 @@ import {Socket, LongPoller} from "phoenix"
 
 // sourced mostly from: https://github.com/muhmi/phoenix_chat_example/blob/master/web/static/js/app.js
 
+let WIDTH = 16;
+let HEIGHT = 16;
+
+let BOX_WIDTH = 512/WIDTH;
+let BOX_HEIGHT = 512/HEIGHT;
+
 class App {
 
   static init(){
+    // This would come from the server
     let state = {
       pixels: [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,12 +30,23 @@ class App {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-     ],
-      width: 16,
-      height: 16
+     ]
     };
+
+    // setup click handler
+    $("#canvas").click(function (e) {
+      let offset = $(this).offset();
+      let x = Math.round((e.pageX - offset.left)/BOX_WIDTH);
+      let y = Math.round((e.pageY - offset.top)/BOX_HEIGHT);
+      App.click(x, y);
+    });
+
     console.log("Alive!");
     App.render(state);
+  }
+
+  static click(x, y) {
+    console.log("click & clack " + x + ", " + y);
   }
 
   static render(state)
@@ -37,24 +55,22 @@ class App {
 
     var palette = ["#000000", "#9d9d9d", "#ffffff", "#be2633", "#e06f8b", "#493c2b", "#a46422", "#eb8931", "#f7e26b", "#2f484e", "#44891a", "#a3ce27", "#1b2632", "#005784", "#31a2f2", "#b2dcef"];
 
-    var boxw = canvas.width/state.width;
-    var boxh = canvas.height/state.height;
-
     ctx.fillStyle = "#fffafa";
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
     var pixels = state.pixels;
 
-    for (var y = 0; y < state.height; y++) {
+    for (var y = 0; y < HEIGHT; y++) {
       var row = pixels[y];
-      for (var x = 0; x < state.width; x++) {
+      for (var x = 0; x < WIDTH; x++) {
         var color = row[x];
         if (color == 0) continue;
         ctx.fillStyle = palette[color];
-        ctx.fillRect(x*boxw,y*boxh,boxw,boxh);
+        ctx.fillRect(x*BOX_WIDTH,y*BOX_HEIGHT,BOX_WIDTH,BOX_HEIGHT);
+
       }
     }
-    
+
   }
 
 }
