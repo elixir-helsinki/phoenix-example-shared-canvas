ba73bd642d613ec44785dc3462ee8ad9071063e6
Roope Kangas <muhmi@iki.fi>
2015-04-25 19:31:21 +0300

    chat and sent pixel

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -33,13 +33,53 @@ class App {
      ]
     };
 
-    // setup click handler
-    $("#canvas").click(function (e) {
-      let offset = $(this).offset();
-      let x = Math.round((e.pageX - offset.left)/BOX_WIDTH);
-      let y = Math.round((e.pageY - offset.top)/BOX_HEIGHT);
-      App.click(x, y);
-    });
+    // start our socket code
+
+    let $messages = $("#messages")
+    let $username  = $("#username")
+    let $input     = $("#message-input")
+
+    let socket = new Socket("ws://" + location.host +  "/ws")
+    socket.connect()
+
+    socket.onClose( e => console.log("CLOSE", e) )
+
+    socket.join("canvas:lobby", {})
+      .receive("ignore", () => console.log("auth error") )
+      .receive("ok", chan => {
+
+        chan.onError( e => console.log("something went wrong", e) )
+        chan.onClose( e => console.log("channel closed", e) )
+
+        $input.off("keypress").on("keypress", e => {
+          if (e.keyCode == 13) {
+            chan.push("new:msg", {user: $username.val(), body: $input.val()})
+            $input.val("")
+          }
+        })
+
+        chan.on("new:msg", msg => {
+          $messages.append(this.messageTemplate(msg))
+          scrollTo(0, document.body.scrollHeight)
+        })
+
+        chan.on("user:entered", msg => {
+          var username = this.sanitize(msg.user || "anonymous")
+          $messages.append(`<br/><i>[${username} entered]</i>`)
+        })
+
+         // setup click handler
+        $("#canvas").click(function (e) {
+          let offset = $(this).offset();
+          let x = Math.round((e.pageX - offset.left) / BOX_WIDTH)
+          let y = Math.round((e.pageY - offset.top) / BOX_HEIGHT)
+          
+          chan.push("new:pixel", {user: $username.val(), x: x, y: y})
+        });
+
+
+      })
+      .after(10000, () => console.log("Connection interruption") )
 
     console.log("Alive!");
     App.render(state);
@@ -51,26 +91,34 @@ class App {
 
   static render(state)
   {
-    var ctx = document.getElementById("canvas").getContext("2d");
+    let ctx = document.getElementById("canvas").getContext("2d")
 
-    var palette = ["#000000", "#9d9d9d", "#ffffff", "#be2633", "#e06f8b", "#493c2b", "#a46422", "#eb8931", "#f7e26b", "#2f484e", "#44891a", "#a3ce27", "#1b2632", "#005784", "#31a2f2", "#b2dcef"];
+    let palette = ["#000000", "#9d9d9d", "#ffffff", "#be2633", "#e06f8b", "#493c2b", "#a46422", "#eb8931", "#f7e26b", "#2f484e", "#44891a", "#a3ce27", "#1b2632", "#005784", "#31a2f2", "#b2dcef"]
 
-    ctx.fillStyle = "#fffafa";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "#fffafa"
+    ctx.fillRect(0,0,canvas.width,canvas.height)
 
-    var pixels = state.pixels;
+    let pixels = state.pixels
 
     for (var y = 0; y < HEIGHT; y++) {
-      var row = pixels[y];
+      let row = pixels[y]
       for (var x = 0; x < WIDTH; x++) {
-        var color = row[x];
-        if (color == 0) continue;
-        ctx.fillStyle = palette[color];
-        ctx.fillRect(x*BOX_WIDTH,y*BOX_HEIGHT,BOX_WIDTH,BOX_HEIGHT);
-
+        let color = row[x]
+        if (color == 0) continue
+        ctx.fillStyle = palette[color]
+        ctx.fillRect(x * BOX_WIDTH, y * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT)
       }
     }
 
+  }
+
+  static sanitize(html) { return $("<div/>").text(html).html() }
+
+  static messageTemplate(msg){
+    let username = this.sanitize(msg.user || "anonymous")
+    let body     = this.sanitize(msg.body)
+
+    return(`<p><a href='#'>[${username}]</a>&nbsp; ${body}</p>`)
   }
 
 }
