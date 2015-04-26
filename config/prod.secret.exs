99539e480ea9a410db7296130f3af1b1b08a1932
Roope Kangas <muhmi@iki.fi>
2015-04-26 21:32:44 +0300

    deploy to heroku...

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,4 +4,4 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :pichat, Pichat.Endpoint,
-  secret_key_base: System.get_env "SECRET_KEY_BASE"
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
