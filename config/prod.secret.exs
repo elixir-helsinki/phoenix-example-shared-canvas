90b2794373064a971e6bf28b95c6fe1dd3c8ab97
Roope Kangas <muhmi@iki.fi>
2015-04-26 20:49:02 +0300

    that should not be in version control

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,4 +4,4 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :pichat, Pichat.Endpoint,
-  secret_key_base: "APYQdJPTiR1gALKOXCAAMo3zJbGbPvZMasOrGeTSXq6zeEpUgz2E7eEeVdFZdh6i"
+  secret_key_base: System.get_env "SECRET_KEY_BASE"
